cf397dd8ccb6c3a4c9846aecc9e3fe0c22aae1eb
S-S-Poojitha <122882364+S-S-Poojitha@users.noreply.github.com>
2024-07-27 04:38:07 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 # URLs of the handcrafted furniture and art images
 image_urls = [
-    "https://cdn.shopify.com/s/files/1/0260/6111/products/Handcrafted_Wooden_Furniture.jpg",
-    "https://cdn.shopify.com/s/files/1/0250/5922/products/Handcrafted_Chair_1.jpg",
-    "https://cdn.pixabay.com/photo/2017/08/30/01/53/handmade-2688934_960_720.jpg",
-    "https://cdn.pixabay.com/photo/2017/08/31/10/32/handcraft-2689458_960_720.jpg"
+    "https://content.jdmagicbox.com/comp/def_content/handicraft-item-dealers/d189618293-handicraft-item-dealers-4-q52mt.jpg",
+    "https://content.jdmagicbox.com/comp/ahmedabad/c8/079pxx79.xx79.101225102212.k3c8/catalogue/toran-art-satellite-ahmedabad-handicraft-item-manufacturers-26qy14nsz2.jpg",
+    "https://th-i.thgim.com/public/incoming/ikjulw/article67682621.ece/alternates/FREE_1200/03Tribals.jpg",
+    "https://m.media-amazon.com/images/I/61D83qJYjDL._AC_UF894,1000_QL80_.jpg"
 ]
 
 # Create a 2x2 grid of images
